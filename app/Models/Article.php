be25e681fbdc14585bb7e504c15f9175c6e4fbcb
Renaud <rdahl@keley-live.com>
2015-06-30 17:34:13 +0200

    Create model

---
+++ app/Models/Article.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: renaud
+ * Date: 30/06/15
+ * Time: 17:07
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    protected $fillable = ['title', 'content'];
+}
